60ed90a12722ea2b7ad40ae2f9c94a2cbb05f184
Lophate <grooverhayden@gmail.com>
2023-12-23 17:24:47 -0500

    git add .

---
+++ src/main/java/loscut/the/maker/commands/commandRepair.java
@@ -0,0 +1,53 @@
+package loscut.the.maker.commands;
+
+import loscut.the.maker.plugin.RepairAnyItem;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+public class commandRepair implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command cmd, String label, String[] strings) {
+
+        if (!(commandSender instanceof Player)) {
+            commandSender.sendMessage(ChatColor.RED + "Only players can do this.");
+            return false;
+        }
+
+        List<?> badItems = RepairAnyItem.getPlugin(RepairAnyItem.class).getConfig().getList("non-fixable");
+
+        Player player = (((Player) commandSender).getPlayer());
+        ItemStack itemInHand = player.getInventory().getItemInMainHand();
+        final Material itemID = itemInHand.getType();
+        short maxDurability = itemID.getMaxDurability();
+        short itemDurability = itemInHand.getDurability();
+
+        if (badItems.contains(String.valueOf(itemID))) {
+            commandSender.sendMessage(ChatColor.RED + "This plugin won't fix that item.");
+            return false;
+        } else if (itemInHand.getDurability() == -1 || itemID.isBlock()) {
+            commandSender.sendMessage(ChatColor.RED + "This can't be fixed.");
+            return false;
+        }
+        // /repair
+        if(cmd.getName().equalsIgnoreCase("repair")) {
+
+            if (player.getInventory().getItemInMainHand() != null && itemID.isItem()) {
+                if (itemDurability != maxDurability && itemDurability != 0 || itemDurability > maxDurability)  {
+                    player.getInventory().getItemInMainHand().setDurability((short) 0);
+                    commandSender.sendMessage(ChatColor.YELLOW + "Your item has been repaired.");
+                } else if (itemDurability == 0) {
+                    commandSender.sendMessage(ChatColor.RED + "This can't be fixed.");
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/loscut/the/maker/plugin/RepairAnyItem.java
@@ -0,0 +1,21 @@
+package loscut.the.maker.plugin;
+
+import loscut.the.maker.commands.commandRepair;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RepairAnyItem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getCommand("repair").setExecutor(new commandRepair());
+        getServer().getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "[RAI]: Enabled");
+        saveConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[RAI]: Disabled");
+    }
+}
